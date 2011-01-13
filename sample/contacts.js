817d52f151c0505cb334d4ad57012f7a1565a1b9
Guy Royse <guy@guyroyse.com>
2011-01-13 01:41:03 -0500

    Refactored to use view for reading add address form

--- sample/contacts.js
+++ sample/contacts.js
@@ -5,29 +5,43 @@ var view = {
 	addResult : function(values) {
 		var table = $('#results').get(0);
 		var row = table.insertRow(-1);
-		for (var i = 0; i < values.length; i++) {
+		for ( var i = 0; i < values.length; i++) {
 			row.insertCell(i).innerHTML = values[i];
-		};
+		}
+	},
+	getName : function() {
+		return $('#name').val();
+	},
+	getEmail : function() {
+		return $('#email').val();
+	},
+	getTwitter : function() {
+		return $('#twitter').val();
+	},
+	getBeverage : function() {
+		return $('#beverage').val();
 	}
 };
 
 $(document).ready(function() {
-	
+
 	$('#add').click(function() {
 		var contact = {
-			name : $('#name').val(),
-			email : $('#email').val(),
-			twitter : $('#twitter').val(),
-			beverage : $('#beverage').val()
+			name : view.getName(),
+			email : view.getEmail(),
+			twitter : view.getTwitter(),
+			beverage : view.getBeverage()
 		};
 		proton.db('contacts').save(contact);
 	});
-	
+
 	$('#search').click(function() {
-		var contacts = proton.db('contacts').find({name: $('#searchName').val()});
+		var contacts = proton.db('contacts').find({
+			name : $('#searchName').val()
+		});
 		displayContacts(contacts);
 	});
-	
+
 	$('#showAll').click(function() {
 		var contacts = proton.db('contacts').all();
 		displayContacts(contacts);
@@ -38,7 +52,11 @@ $(document).ready(function() {
 function displayContacts(contacts) {
 	view.clearResults();
 	var table = $('#results').get(0);
-	contacts.order({name: proton.ascending}).forEach(function(contact) {
-		view.addResult([contact.name, contact.email, contact.twitter, contact.beverage]);
-	});
+	contacts.order({
+		name : proton.ascending
+	}).forEach(
+			function(contact) {
+				view.addResult([ contact.name, contact.email, contact.twitter,
+						contact.beverage ]);
+			});
 }
